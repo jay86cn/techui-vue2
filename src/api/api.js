308f663403c90d110e95850cd296f0a91a1e209c
ayin86cn <ayin86cn@gmail.com>
2023-07-15 07:49:10 +0800

    clean

--- src/api/api.js
+++
@@ -1,6 +0,0 @@
-import { http,httpLocal,fetchLocl} from "./request"
-
-//接口demo
-export const map = (url,params) => { return fetchLocl("get",url+params)}
-export const loginApi = params => { return http("post",'/test/ste/', params)} 
-export const httpGaugeTriple = params => { return http("get",'./static/tempData/gaugeTriple.json', params)} 
